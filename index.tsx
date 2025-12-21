fddaca1338d2f30867aa8f44083ba026785ee0da
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2025-12-21 21:53:09 +0400

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  // نستخدم Render مباشر بدون StrictMode لحل مشاكل التداخل في بعض البيئات
   root.render(<App />);
 } else {
   console.error("Root container not found");
