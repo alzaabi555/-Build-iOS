ffe1c71d654c832ee73a9c6d505282349c7a608a
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-20 15:35:26 +0400

    Update StudentList.tsx

--- components/StudentList.tsx
+++ components/StudentList.tsx
@@ -581,7 +581,7 @@ const StudentList: React.FC<StudentListProps> = ({
         
         {/* ================= FIXED HEADER ================= */}
         <header 
-            <header className={`shrink-0 z-40 px-4 pt-[calc(env(safe-area-inset-top)+1.5rem)] pb-6 shadow-sm w-full transition-all duration-300 ${isRamadan ? 'bg-[#0f172a]/80 border-b border-white/10 text-white backdrop-blur-xl' : 'bg-[#446A8D] text-white border-b border-[#3b5d7c]'}`}>
+         <header className={`shrink-0 z-40 px-4 pt-[calc(env(safe-area-inset-top)+1.5rem)] pb-6 shadow-sm w-full transition-all duration-300 ${isRamadan ? 'bg-[#0f172a]/80 border-b border-white/10 text-white backdrop-blur-xl' : 'bg-[#446A8D] text-white border-b border-[#3b5d7c]'}`}>
             style={{ WebkitAppRegion: 'drag' } as any}
         >
             <div className="flex justify-between items-center mb-6">
