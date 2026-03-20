8ca9ae8e659e36f751229bb40f761df8ca376c54
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-20 17:41:43 +0400

    Update StudentList.tsx

--- components/StudentList.tsx
+++ components/StudentList.tsx
@@ -579,9 +579,10 @@ const StudentList: React.FC<StudentListProps> = ({
     return (
    <div className={`flex flex-col h-full space-y-6 pb-24 md:pb-8 overflow-hidden relative ${isRamadan ? 'text-white' : 'text-slate-800'} ${dir === 'rtl' ? 'text-right' : 'text-left'}`} dir={dir}>
             
-   <header className={`shrink-0 z-40 px-2 pt-[calc(env(safe-area-inset-top)+0.5rem)] pb-2 w-full transition-all duration-300 bg-transparent ${isRamadan ? 'text-white' : 'text-slate-800'}`}>
-        style={{ WebkitAppRegion: 'drag' } as any}
-    >
+ <header 
+    className={`shrink-0 z-40 px-2 pt-[calc(env(safe-area-inset-top)+0.5rem)] pb-2 w-full transition-all duration-300 bg-transparent ${isRamadan ? 'text-white' : 'text-slate-800'}`}
+    style={{ WebkitAppRegion: 'drag' } as any}
+>
             <div className="flex justify-between items-center mb-6">
                 <div className="flex items-center gap-3">
                     <div className="bg-white/10 p-2 rounded-xl border border-white/20">
