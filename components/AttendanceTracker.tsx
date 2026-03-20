ad33a7326558b794734b81388fd6ff37e60b9f60
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-20 15:33:19 +0400

    Update AttendanceTracker.tsx

--- components/AttendanceTracker.tsx
+++ components/AttendanceTracker.tsx
@@ -206,7 +206,7 @@ const AttendanceTracker: React.FC<AttendanceTrackerProps> = ({ students, classes
         
         {/* ================= FIXED HEADER ================= */}
             <header 
-             <header className={`shrink-0 z-40 px-4 pt-[calc(env(safe-area-inset-top)+1.5rem)] pb-6 shadow-sm w-full transition-all duration-300 ${isRamadan ? 'bg-[#0f172a]/80 border-b border-white/10 text-white backdrop-blur-xl' : 'bg-[#446A8D] text-white border-b border-[#3b5d7c]'}`}>
+          <header className={`shrink-0 z-40 px-4 pt-[calc(env(safe-area-inset-top)+1.5rem)] pb-6 shadow-sm w-full transition-all duration-300 ${isRamadan ? 'bg-[#0f172a]/80 border-b border-white/10 text-white backdrop-blur-xl' : 'bg-[#446A8D] text-white border-b border-[#3b5d7c]'}`}>
                 style={{ WebkitAppRegion: 'drag' } as any}
             >
                 <div className="flex justify-between items-center gap-3 mb-5">
