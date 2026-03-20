36e5145eda8aba706a7d72c7df863874813b3982
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-20 22:46:11 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { AppProvider, useApp } from './context/AppContext';
 import { ThemeProvider } from './context/ThemeContext';
 import {
   LayoutDashboard, Users, CalendarCheck, BarChart3,
-  Settings as SettingsIcon, Info, FileText, BookOpen, Medal, Loader2, Networ, CheckSquare
+  Settings as SettingsIcon, Info, FileText, BookOpen, Medal, Loader2, Network, CheckSquare
 } from 'lucide-react';
 
 import { App as CapacitorApp } from '@capacitor/app';
