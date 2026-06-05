18ceb4b394e366d5d1257677c4fd0bb6dec1d773
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-06-05 20:20:34 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
+   "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./context/**/*.{js,ts,jsx,tsx}",
     "./theme/**/*.{js,ts,jsx,tsx}",
